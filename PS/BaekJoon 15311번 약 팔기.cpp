adef3180f2fc64386d5e3b87627715612bebe0a3
Choi Han jun <gkswns3708@naver.com>
2020-10-29 16:49:14 +0900

    BaekJoon 15311번 약 팔기

---
+++ PS/BaekJoon 15311번 약 팔기.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int main() {
+	cout << 2000 << " ";
+	for (int i = 0; i < 1000; i++) {
+		cout << 1 << " ";
+	}
+	for (int i = 0; i < 1000; i++) {
+		cout << 1000 << " ";
+	}
+	int a; cin >> a;
+	return 0;
+}
