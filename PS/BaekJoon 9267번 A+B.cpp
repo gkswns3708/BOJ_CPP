1023d820ba99665e105063ca1902ba7dcb0e1eed
Choi Han jun <gkswns3708@naver.com>
2020-11-20 02:58:06 +0900

    임시

---
+++ PS/BaekJoon 9267번 A+B.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+#pragma warning(disable:4996)
+
+using namespace std;
+
+bool cal(int a,int b,int s) {
+	if (a == 0 || b == 0)return s == 0;
+	if (a == 0) return s % b == 0;
+	if (b == 0) return s % a == 0;
+	ll g, y, x =gcd(a, b);
+}
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	ll a, b, s; cin >> a >> b >> s;
+	ll max
+	return 0;
+}
