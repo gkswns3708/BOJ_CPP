0c4046c968c0cbfda69792215f5247589df98a04
Choi Han jun <gkswns3708@naver.com>
2020-11-23 20:08:45 +0900

    BaekJoon 3190번 뱀

--- PS/BaekJoon 20164번 홀수 홀릭 호석.cpp
+++ PS/BaekJoon 20164번 홀수 홀릭 호석.cpp
@@ -5,42 +5,30 @@ using namespace std;
 pair<int, int> recul(string s) {
 	int length = s.length();
 	int cnt = 0;
+	for (int i = 0; i < length; i++)
+		if ((s[i] - '0') % 2) cnt++;
 	int mn = INT_MAX;
 	int mx = INT_MIN;
-	if (length == 1) {
-		if ((s[0] - '0') % 2) cnt++;
-		mn = 0;
-		mx = 0;
-		//cout << s << "ÀÏ ¶§ È¦ ¼ö °¹¼ö : " << cnt << '\n';
-	}
+	if (length == 1)
+		mn = mx = 0;
 	else if (length == 2) {
-		if ((s[0] - '0') % 2) cnt++;
-		if ((s[1] - '0') % 2) cnt++;
 		pair<int, int> nowpair = recul(to_string(s[0] - '0' + s[1] - '0'));
 		mn = min(mn, nowpair.first);
 		mx = max(mx, nowpair.second);
-		//cout << s << "ÀÏ ¶§ È¦ ¼ö °¹¼ö : " << cnt << '\n';
 	}
 	else {
 		int beforecnt = 0;
-		//¹®ÀÚ¿­ÀÇ ¼Ò¼ö °¹¼ö
-		for (int i = 0; i < length; i++)
-			if ((s[i] - '0') % 2) cnt++;
-		//cout << s << "ÀÏ ¶§ È¦ ¼ö °¹¼ö : " << cnt << '\n';
-		//ÇÕÀÇ ¼Ò¼ö °¹¼ö
 		for (int i = 1; i <= length - 2; i++) {
 			for (int j = i + 1; j <= length - 1; j++) {
 				string s1, s2, s3;
 				s1 = s.substr(0, i);
 				s2 = s.substr(i, j - i);
 				s3 = s.substr(j, length - j);
-				//cout << s1 << " " << s2 << " " << s3 << "\n";
 				int sum = 0;
 				sum += stoi(s1);
 				sum += stoi(s2);
 				sum += stoi(s3);
 				pair<int, int> nowpair = recul(to_string(sum));
-				//cout << nowpair.first << " " << nowpair.second << "\n";
 				mn = min(mn, nowpair.first);
 				mx = max(mx, nowpair.second);
 			}
---
+++ PS/BaekJoon 3190번 뱀.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int arr[105][105];
+int N,K,L;
+
+int dx[] = { 1,0,-1,0 };
+int dy[] = { 0,1,0,-1 };
+bool poss = true;
+deque<tuple<int, int, int>> deq;
+deque<pair<int, int>> dirinfo;
+
+bool possible(int y, int x) {
+	if (arr[y][x] != 2 && y >= 1 && y <= N && x >= 1 && x <= N) return true;
+	else {
+		poss = false;
+		return false;
+	}
+}
+
+void move() {
+	auto [y, x, dir] = deq.front();
+	int ny = y + dy[dir];
+	int nx = x + dx[dir];
+	if (possible(ny, nx)) {
+		deq.push_front({ ny,nx,dir });
+	}
+}
+
+int solved() {
+	deq.push_back({ 1,1,0 });
+	int time = 0;
+	while (poss) {
+		time++;
+		move();
+		auto [y, x, dir] = deq.front();
+		if (arr[y][x] == 0) {
+			auto [ly, lx, dir] = deq.back();
+			arr[ly][lx] = 0;
+			arr[y][x] = 2;
+			deq.pop_back();
+		}
+		
+
+		
+		if (!dirinfo.empty() && time == dirinfo.front().first) {
+			auto& [y, x, dir] = deq.front();
+			if (dirinfo.front().second == 1)
+				dir = (dir + 1) % 4;
+			else
+				dir = (dir + 3) % 4;
+			dirinfo.pop_front();
+		}
+	}
+	return time;
+}
+
+int main() {
+	fastio;
+	cin >> N >> K;
+	for (int i = 0; i < K; i++) {
+		int y, x; cin >> y >> x;
+		arr[y][x] = 1;
+	}
+	cin >> L;
+	for (int i = 0; i < L; i++) {
+		int time; char dir; cin >> time >> dir;
+		if (dir == 'L') dirinfo.push_back({ time,0 });
+		else dirinfo.push_back({ time,1 });
+	}
+	cout << solved();
+	return 0;
+}
