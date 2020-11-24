651f7bdd7e3c4db5662cca1ef4fde04af93f0b69
Choi Han jun <gkswns3708@naver.com>
2020-11-25 02:59:21 +0900

    BaekJoon 12837번 가계부

---
+++ PS/BaekJoon 12837번 가계부.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+typedef long long int ll;
+
+struct ST {
+	ll tree[1 << 21];
+	int size = 1 << 20;
+
+	void update(int idx, int val) {
+		--idx |= size; tree[idx] += val;
+		while (idx >>= 1)
+			tree[idx] = tree[idx << 1] + tree[idx << 1 | 1];
+	}
+	ll query(int l, int r) {
+		--l |= size; --r |= size;
+		ll sum = 0;
+		while (l <= r) {
+			if (l & 1) sum += tree[l++];
+			if (~r & 1) sum += tree[r--];
+			l >>= 1;
+			r >>= 1;
+		}
+		return sum;
+	}
+}ST;
+
+int main() {
+	fastio;
+	int n, m; cin >> n >> m;
+	while (m--) {
+		int type, idx, value; cin >> type >> idx >> value;
+		if (type == 1) 	ST.update(idx, value);
+		else cout << ST.query(idx, value) << "\n";
+	}
+	return 0;
+}
