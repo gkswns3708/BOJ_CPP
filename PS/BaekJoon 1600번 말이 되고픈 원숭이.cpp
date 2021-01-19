cb70cc8a81fb7cc6788b3304794cd4393a65194f
Choi Han jun <gkswns3708@naver.com>
2021-01-20 01:18:26 +0900

    BaekJoon 1600번 말이 되고픈 원숭이

---
+++ PS/BaekJoon 1600번 말이 되고픈 원숭이.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+int K, H, W;
+int arr[200 + 1][200 + 1];
+bool visited[200 + 1][200 + 1][30+1];
+int dy[] = { -1,-2,-2,-1,1,2,2,1 };
+int dx[] = { -2,-1,1,2,2,1,-1,-2 };
+
+bool possible(int y, int x, int k) {
+	if (visited[y][x][k] == false && y >= 0 && y < H && x >= 0 && x < W && k<=K && arr[y][x] != 1) return true;
+	else return false;
+}
+
+void input() {
+	cin >> K >> W >> H;
+	for (int i = 0; i < H; i++) for (int j = 0; j < W; j++) cin >> arr[i][j];
+}
+
+void solution() {
+	int ans = 0;
+	bool check = false;
+	queue<tuple<int, int, int>> que;
+	que.push({ 0,0,0 });
+	visited[0][0][0] = true;
+	while (!que.empty()) {
+		int size = que.size();
+		for (int n = 0; n < size; n++) {
+			auto [y, x, k] = que.front();
+			if (y == H - 1 && x == W - 1) check = true;
+			if (check) break;
+			que.pop();
+			for (int i = 0; i < 4; i++) {
+				int nx = x + "1012"[i] - '1';
+				int ny = y + "2101"[i] - '1';
+				if (possible(ny, nx, k)) {
+					que.push({ ny,nx,k });
+					visited[ny][nx][k] = true;
+				}
+			}
+			for (int i = 0; i < 8; i++) {
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				if (possible( ny,nx,k+1)) {
+					que.push({ ny, nx, k+1 });
+					visited[ny][nx][k + 1] = true;
+				}
+
+			}
+		}
+		if (check) break;
+		ans++;
+	}
+	if (check) cout << ans;
+	else cout << -1;
+}
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cout.tie(0);
+	cin.tie(0);
+	input();
+	solution();
+	return 0;
+}
--- PS/practice.cpp
+++ PS/practice.cpp
@@ -1,25 +1,11 @@
-#include <stdio.h>
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
 
 int main() {
-	int p[10][10] = { 0, };
-
-	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j <= i; j++) {
-			if (j == 0) p[i][j] = 1;
-			else if (i == j) p[i][j] = 1;
-			else {
-				//...중간 삼각형의 값.
-				p[i][j] = p[i - 1][j - 1] + p[i - 1][j];
-			}
-		}
-	}
-
-	for (int i = 0; i < 10; i++) {
-		for (int j = 0; j <= i; j++) {
-			printf("%d ", p[i][j]);
-		}
-		printf("\n");
-	}
+	ios_base::sync_with_stdio(0);
+	cout.tie(0);
+	cin.tie(0);
 
 	return 0;
 }
