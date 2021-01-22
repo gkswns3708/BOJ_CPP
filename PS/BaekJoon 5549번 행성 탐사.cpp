09a2ae71ed06f291fafb2de0887e3298541602e5
Choi Han jun <gkswns3708@naver.com>
2021-01-23 02:58:19 +0900

    BaekJoon 5549번 행성 탐사

---
+++ PS/BaekJoon 5549번 행성 탐사.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+
+//가로 N, 세로 M
+int W, H, K;
+char arr[1000 + 2][1000 + 2];
+//J, O, I
+tuple<int, int, int> ps[1000 + 1][1000 + 1];
+
+
+void input() {
+	cin >> H >> W >> K;
+	for (int i = 0; i < H; i++) for (int j = 0; j < W; j++) {
+		cin >> arr[i][j];
+		if()
+	}
+}
+
+void solution() {
+	for (int i = 0; i < H; i++) {
+		for (int j = 0; j < W; j++) {
+			if (arr[i][j] == 'J') {
+				ps
+			}
+		}
+	}
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cout.tie(0);
+	cin.tie(0);
+
+	input();
+	solution();
+
+	return 0;
+}
