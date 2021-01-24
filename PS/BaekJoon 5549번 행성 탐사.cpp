77d0d254befc529bfce2c5c7e3b04c1487d0d8a5
Choi Han jun <gkswns3708@naver.com>
2021-01-24 12:07:36 +0900

    BaekJoon 5549번 행성 탐사

--- PS/BaekJoon 5549번 행성 탐사.cpp
+++ PS/BaekJoon 5549번 행성 탐사.cpp
@@ -6,22 +6,22 @@ typedef long long int ll;
 int W, H, K;
 char arr[1000 + 2][1000 + 2];
 //J, O, I
-tuple<int, int, int> ps[1000 + 1][1000 + 1];
+int ps[1000 + 1][1000 + 1][3];
+int conv[128];
 
 
 void input() {
 	cin >> H >> W >> K;
-	for (int i = 0; i < H; i++) for (int j = 0; j < W; j++) {
-		cin >> arr[i][j];
-		if()
-	}
+	for (int i = 0; i < H; i++) for (int j = 0; j < W; j++) cin >> arr[i][j];
+	conv['J'] = 0;	conv['O'] = 1;	conv['I'] = 2;
+	
 }
 
 void solution() {
 	for (int i = 0; i < H; i++) {
 		for (int j = 0; j < W; j++) {
-			if (arr[i][j] == 'J') {
-				ps
+			for (int k = 0; k < 3; k++) {
+				ps[i][j][k] = ps[i][j][k] - ps[i][j][k] - ps[i][j][k] - ps[i][j][k];
 			}
 		}
 	}
