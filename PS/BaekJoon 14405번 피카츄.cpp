fba7b02fcf1ea1e6dd17a93892ad9ea56a2632c0
Choi Han jun <gkswns3708@naver.com>
2021-01-30 16:17:02 +0900

    BaekJoon 14405번 피카츄

--- PS/BaekJoon 14405번 피카츄.cpp
+++ PS/BaekJoon 14405번 피카츄.cpp
@@ -3,25 +3,18 @@
 #define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 using namespace std;
 
-#define X first
-#define Y second
-#define sz(v) (int)(v).size()
-#define all(v) (v).begin(), (v).end()
-#define rall(v) (v).rbegin(), (v).rend()
-#define compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
-#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
-
-using ll = long long;
-using ull = unsigned long long;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-
-template<typename T> istream& operator>> (istream& in, vector<T>& i) { for (auto& i : i) in >> i; return in; }
-template<typename T> istream& operator>> (istream& in, pair<T, T>& i) { in >> i.X >> i.Y; return in; }
-
 int main() {
 	fastio;
 	string s; cin >> s;
-	regex r(R"((pi|ka|chu)+)");
+	regex r(R"(^((pi)*(ka)*(chu)*){1,})");
+	sregex_iterator it;
+	smatch m;
+	vector<string> v;
+	bool found = regex_search(s, m, r);
+	for (auto it = sregex_iterator(s.begin(), s.end(), r);; it++) {
+		v.push_back(it->str());
+	}
+	for (auto a : v)
+		cout << a << "\n";
 	regex_match(s, r) ? cout << "YES" : cout << "NO";
 }
