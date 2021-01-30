d685586a32b956376584ea65c6e3ddc6cb404671
Choi Han jun <gkswns3708@naver.com>
2021-01-30 14:53:09 +0900

    BaekJoon 1543번 문서 검색

---
+++ PS/BaekJoon 1543번 문서 검색.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+using namespace std;
+
+#define X first
+#define Y second
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+template<typename T> istream& operator>> (istream& in, vector<T>& i) { for (auto& i : i) in >> i; return in; }
+template<typename T> istream& operator>> (istream& in, pair<T, T>& i) { in >> i.X >> i.Y; return in; }
+
+int main() {
+	fastio;
+	string s, s1;
+	getline(cin, s);
+	getline(cin, s1);
+
+	auto pos = s.find(s1);
+	int ans = 0;
+	while (pos != string::npos) {
+		ans++;
+		pos = s.find(s1, pos + s1.size());
+	}
+	cout << ans;
+
+}
