b35df3c0dfbca22c37f34bab8d3dbcfb60147b6f
Choi Han jun <gkswns3708@naver.com>
2021-02-06 23:07:14 +0900

    BaekJoon 1738번 골목길 - 2

--- PS/BaekJoon 1738번 골목길 - 2.cpp
+++ PS/BaekJoon 1738번 골목길 - 2.cpp
@@ -21,78 +21,71 @@ template<typename T> istream& operator>> (istream& in, vector<T>& i) { for (auto
 template<typename T> istream& operator>> (istream& in, pair<T, T>& i) { in >> i.X >> i.Y; return in; }
 
 int N, M;
-ll parent[100 + 1];
 vector<pll> edge[100 + 1];
+vector<ll> dist(100+1);
+vector<ll> previous(100 + 1);
 vector<ll> rev[100 + 1];
-ll dist[100 + 1];
-int previous[100 + 1];
 bool visited[100 + 1];
 bool cycle;
 
 
 void bellman_ford() {
-	for (int k = 1; k <= N; k++) {
+	for (int k = 0; k < N; k++) {
 		for (int i = 1; i <= N; i++) {
 			for (int j = 0; j < edge[i].size(); j++) {
-				auto [to, ndist] = edge[i][j];
-				if (dist[i] != INF && dist[to] > dist[i] + ndist) {
-					dist[to] = dist[i] + ndist;
-					previous[to] = i;
-					if (k == N && visited[to])
-						cycle = 1;
+				auto [nw, ndist] = edge[i][j];
+				if (dist[i] != -INF && dist[nw] < dist[i] + ndist) {
+					dist[nw] = dist[i] + ndist;
+					previous[nw] = i;
+					if (k == N - 1 && visited[nw]) cycle = 1;
 				}
 			}
 		}
 	}
 }
 
-
 void input() {
 	cin >> N >> M;
 	for (int i = 0; i < M; i++) {
 		ll u, v, w; cin >> u >> v >> w;
-		//*-1해서 넣음 이유 찾기.
-		edge[u].push_back({ v,-w });
+		edge[u].push_back({ v,w });
 		rev[v].push_back(u);
 	}
 }
 
-
-
 void solution() {
+	fill(dist.begin(), dist.end(), -INF);
+	dist[1] = 0;
+
 	queue<int> que;
 	que.push(N);
 	visited[N] = 1;
 	while (!que.empty()) {
 		int now = que.front();
 		que.pop();
-		for (auto nxt : rev[now]) {
-			if (!visited[nxt]) {
-				visited[nxt] = 1;
-				que.push(nxt);
+		for (auto nw : rev[now]) {
+			if (!visited[nw]) {
+				visited[nw] = 1;
+				que.push(nw);
 			}
 		}
 	}
 
-	for (int i = 2; i <= N; i++)
-		dist[i] = INF;
-	
 	bellman_ford();
 
-	if (cycle) cout << -1 << "\n";
+	if (cycle) cout << -1;
 	else {
-
 		vector<int> ans;
-		int idx = N;
-		while (idx != 1) {
-			ans.push_back(idx);
-			idx = previous[idx];			
+		int pos = N;
+		while(pos != 1) {
+			ans.push_back(pos);
+			pos = previous[pos];
 		}
-		cout << 1 << " ";
-		for (vector<int>::reverse_iterator riterpos = ans.rbegin(); riterpos != ans.rend(); riterpos++) cout << *riterpos << " ";
+		ans.push_back(1);
+		for (vector<int>::reverse_iterator riterpos = ans.rbegin(); riterpos != ans.rend(); riterpos++)
+			cout << *riterpos << " ";
 	}
 }
-
 
 int main() {
 	fastio;
---
+++ PS/BaekJoon 5430번 AC.cpp
@@ -0,0 +1,82 @@
+#include<bits/stdc++.h>
+#include <regex>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+using namespace std;
+
+#define X first
+#define Y second
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+template<typename T> istream& operator>> (istream& in, vector<T>& i) { for (auto& i : i) in >> i; return in; }
+template<typename T> istream& operator>> (istream& in, pair<T, T>& i) { in >> i.X >> i.Y; return in; }
+
+string s,s1;
+int N;
+
+void solution() {
+	bool possible = false;
+	bool flag = false;
+	deque<int> deq;
+	string temp;
+	for (int i = 0; i < s1.size(); i++) {
+		if (s1[i] >= '0' && s1[i] <= '9') temp += s1[i];
+		else {
+			if(!temp.empty())deq.push_back(stoi(temp));
+			temp = "";
+		}
+	}
+	for (int i = 0; i < s.length(); i++) {
+		if (s[i] == 'R') flag = !flag;
+		else {
+			if (deq.empty()) {
+				possible = true;
+				break;
+			}
+			else {
+				if (flag) deq.pop_back();
+				else deq.pop_front();
+			}
+		}
+	}
+	if (possible) cout << "error\n";
+	else {
+		cout << "[";
+		if (!deq.empty()) {
+			if (flag) {
+				for (int i = deq.size()-1; i > 0 ; i--) cout << deq[i] << ",";
+				cout << deq[0]<<"";
+			}
+			else {
+				for (int i = 0; i < deq.size() - 1; i++) cout << deq[i] << ",";
+				cout << deq[deq.size() - 1];
+			}
+		}
+		cout << "]\n";
+	}
+}
+
+
+void input() {
+	int TC; cin >> TC;
+	while (TC--) {
+		cin >> s;
+		cin >> N;
+		cin >> s1;
+		solution();
+	}
+}
+
+
+int main() {
+	fastio;
+	input();
+}
