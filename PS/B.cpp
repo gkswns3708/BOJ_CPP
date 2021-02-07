7feefc636d210bcbb9307784fa31f36695e90c7c
Choi Han jun <gkswns3708@naver.com>
2021-02-08 01:35:54 +0900

    B

--- PS/B.cpp
+++ PS/B.cpp
@@ -1,16 +1,46 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
 using namespace std;
-typedef long long int ll;
+
+#define INF 987654321
 
 int main() {
-	ios_base::sync_with_stdio(0);
-	cout.tie(0);
-	cin.tie(0);
-	int TC; cin >> TC;
-	while (TC--) {
-		int N, L; cin >> N >> L;
-		
-		
+	fastio;
+	int N;cin >> N;
+	vector<int> vec(N + 10, -INF);
+	int st = 1, end = N;
+	vec[st] = INF;
+	vec[end] = INF;
+	int mid = (st + end) / 2;
+	cout << "? " << mid<<"\n";
+	cout.flush();
+	cin >> vec[mid];
+	//mid를 한번 받고 시작.
+	while (st+2 < end) {
+		int now;
+		if (end - mid < mid - st) now = (mid + st) / 2;
+		else now = (mid + end) / 2;
+		cout << "? " << now<<"\n";
+		cout.flush();
+		cin >> vec[now];
+		if (vec[now] < vec[mid]) {
+			if (mid - st > end - mid) {
+				end = mid;
+				mid = now;
+			}
+			else {
+				st = mid;
+				mid = now;
+			}
+		}
+		else {
+			if (mid - st > end - mid) {
+				st = now;
+			}
+			else {
+				end = now;
+			}
+		}
 	}
-	return 0;
+	cout << "! " << (st + end) / 2 << endl;
 }
