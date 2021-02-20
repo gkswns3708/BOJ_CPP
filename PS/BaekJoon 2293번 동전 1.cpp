35cd431a222cf6d9351c019ea167a0d2bdd0af47
Choi Han jun <gkswns3708@naver.com>
2021-02-20 23:31:24 +0900

    BaekJoon 2293번 동전 1

--- PS/BaekJoon 2293번 동전 1.cpp
+++ PS/BaekJoon 2293번 동전 1.cpp
@@ -11,14 +11,9 @@ int main()
 	cin >> n >> k;
 	vec.resize(n);
 	for (int i = 0; i < n; i++) cin >> vec[i];
-	sort(vec.begin(), vec.end());
 	arr[0] = 1;
-	for (int i = 0; i < n; i++) {
-		for (int j = vec[i]; j <= k; j++) {
+	for (int i = 0; i < n; i++) 
+		for (int j = 0 + vec[i]; j <= k; j++) 
 			if (j - vec[i] >= 0) arr[j] += arr[j - vec[i]];
-		}
-	}
 	cout << arr[k];
 }
-//top-down
-//bottom-up
