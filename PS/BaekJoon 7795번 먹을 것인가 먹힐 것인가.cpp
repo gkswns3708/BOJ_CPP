162c5acc9dfe5f10daf931056c8d3ba9ad61c200
Choi Han jun <gkswns3708@naver.com>
2021-02-24 21:58:13 +0900

    BaekJoon 7795번 먹을 것인가 먹힐 것인가

---
+++ PS/BaekJoon 7795번 먹을 것인가 먹힐 것인가.cpp
@@ -0,0 +1,153 @@
+#include<bits/stdc++.h>
+#include <regex>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+using namespace std;
+
+#define X first
+#define Y second
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+template<typename T> istream& operator>> (istream& in, vector<T>& i) { for (auto& i : i) in >> i; return in; }
+template<typename T> istream& operator>> (istream& in, pair<T, T>& i) { in >> i.X >> i.Y; return in; }
+const int SZ = 1 << 20;
+char read_buf[SZ], write_buf[SZ];
+
+namespace IO {
+	int read_idx, next_idx, write_idx;
+	inline bool is_blank(char c) { return c == ' ' || c == '\n' || c == '\t' || c == '\v' || c == '\f' || c == '\r'; }
+	inline bool is_end(char c) { return c == '\0'; }
+	inline char readChar() {
+		if (read_idx == next_idx) {
+			next_idx = fread(read_buf, sizeof(char), SZ, stdin);
+			if (next_idx == 0) return 0;
+			read_idx = 0;
+		}
+		return read_buf[read_idx++];
+	}
+	inline int readInt() {
+		int ret = 0;
+		char cur = readChar();
+		bool flag = 0;
+		while (is_blank(cur)) cur = readChar();
+		if (cur == '-') {
+			flag = 1;
+			cur = readChar();
+		}
+		while (!is_blank(cur) && !is_end(cur)) {
+			ret = 10 * ret + cur - '0';
+			cur = readChar();
+		}
+		return flag ? -ret : ret;
+	}
+	inline long long readLL() {
+		long long ret = 0;
+		char cur = readChar();
+		bool flag = 0;
+		while (is_blank(cur)) cur = readChar();
+		if (cur == '-') {
+			flag = 1;
+			cur = readChar();
+		}
+		while (!is_blank(cur) && !is_end(cur)) {
+			ret = 10 * ret + cur - '0';
+			cur = readChar();
+		}
+		return flag ? -ret : ret;
+	}
+	inline string readString() {
+		string ret;
+		char cur = readChar();
+		while (is_blank(cur)) cur = readChar();
+		while (!is_blank(cur) && !is_end(cur)) {
+			ret.push_back(cur);
+			cur = readChar();
+		}
+		return ret;
+	}
+	template <typename T>
+	inline int getSize(T n) {
+		int ret = 1;
+		if (n < 0) n = -n;
+		while (n >= 10) {
+			ret++;
+			n /= 10;
+		}
+		return ret;
+	}
+	inline void bflush() {
+		fwrite(write_buf, sizeof(char), write_idx, stdout);
+		write_idx = 0;
+	}
+	inline void writeChar(char c) {
+		if (write_idx == SZ) bflush();
+		write_buf[write_idx++] = c;
+	}
+	inline void newLine() { writeChar('\n'); }
+	inline void writeInt(int n) {
+		int sz = getSize(n);
+		if (write_idx + sz >= SZ) bflush();
+		if (n < 0) {
+			write_buf[write_idx++] = '-';
+			n = -n;
+		}
+		for (int i = sz - 1; i >= 0; i--) {
+			write_buf[write_idx + i] = n % 10 + '0';
+			n /= 10;
+		}
+		write_idx += sz;
+	}
+	inline void writeLL(long long n) {
+		int sz = getSize(n);
+		if (write_idx + sz >= SZ) bflush();
+		if (n < 0) {
+			write_buf[write_idx++] = '-';
+			n = -n;
+		}
+		for (int i = sz - 1; i >= 0; i--) {
+			write_buf[write_idx + i] = n % 10 + '0';
+			n /= 10;
+		}
+		write_idx += sz;
+	}
+	inline void writeString(string s) {
+		for (auto& c : s) writeChar(c);
+	}
+}
+using namespace IO;
+vector<int> A, B;
+int N, M;
+void input() {
+	N = readInt(), M = readInt();
+	A.resize(N);
+	B.resize(M);
+	for (int i = 0; i < N; i++) A[i] = readInt();
+	for (int i = 0; i < M; i++) B[i] = readInt();
+}
+
+void solution() {
+	sort(all(B));
+	int cnt = 0;
+	int st = -1, ed = B.size() + 1;
+	for (int i = 0; i < N; i++) cnt += (lower_bound(all(B), A[i]) - B.begin());
+	writeInt(cnt);
+	newLine();
+	bflush();
+}
+
+int main() {
+	fastio;
+	int TC; TC = readInt();
+	while (TC--) {
+		input();
+		solution();
+	}
+}
