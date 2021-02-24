f45076d9263c1f03745ee62b87a67d423cc4e2dd
Choi Han jun <gkswns3708@naver.com>
2021-02-25 01:57:16 +0900

    BaekJoon 2776번 암기왕

---
+++ PS/BaekJoon 2776번 암기왕.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+#include <regex>
+#define fastio ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+using namespace std;
+
+#define X first
+#define Y second
+#define sz(v) (int)(v).size()
+#define all(v) (v).begin(), (v).end()
+#define rall(v) (v).rbegin(), (v).rend()
+#define compress(v) sort(all(v)), (v).erase(unique(all(v)), (v).end())
+#define OOB(x, y) ((x) < 0 || (x) >= n || (y) < 0 || (y) >= m)
+
+using ll = long long;
+using ull = unsigned long long;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+template<typename T> istream& operator>> (istream& in, vector<T>& i) { for (auto& i : i) in >> i; return in; }
+template<typename T> istream& operator>> (istream& in, pair<T, T>& i) { in >> i.X >> i.Y; return in; }
+
+map<int, int> mp;
+int N,M;
+void input() {
+	mp.clear();
+	cin >> N;
+	for (int i = 0; i < N; i++) {
+		int a; cin >> a; mp[a] = 1;
+	}
+}
+
+void solution() {
+	cin >> M;
+	bool check = false;
+	for (int i = 0; i < M; i++) {
+		int a; cin >> a;
+		if (mp.find(a) == mp.end()) cout << 0 << "\n";
+		else cout << 1 << "\n";
+	}
+}
+
+int main() {
+	fastio;
+	int TC; cin >> TC;
+	while (TC--) {
+		input();
+		solution();
+	}
+}
