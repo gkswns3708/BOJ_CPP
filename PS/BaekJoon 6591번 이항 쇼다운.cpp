2f04db58203ba6090255c7284ff7ecc957b02bcb
Choi Han jun <gkswns3708@naver.com>
2021-03-02 02:58:02 +0900

    BaekJoon 6591번 이항 쇼다운

---
+++ PS/BaekJoon 6591번 이항 쇼다운.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+typedef long long int ll;
+
+ll a, b;
+
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
+	while (cin >>a  >> b && (a != 0 &&b  != 0))  {
+		ll ans = 1, div = 1;
+		b = min(b, a - b);
+		while (b--) {
+			ans *= a--;
+			ans /= div++;
+		}
+		cout << ans << "\n";
+		}
+	return 0;
+}
