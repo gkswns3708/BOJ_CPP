f9ca2b615b2e1075fcc50d6019325ba7147bdefc
Choi Han jun <gkswns3708@naver.com>
2021-03-10 10:35:41 +0900

    BaekJoon 11441번 합 구하기

--- PS/BaekJoon 11441번 합 구하기.cpp
+++ PS/BaekJoon 11441번 합 구하기.cpp
@@ -1,25 +1,26 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+
+#define int long long int
 using namespace std;
 
-#define INF 987654321
-#define int long long int
+
 
 int32_t main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	int N, K; cin >> N >> K;
-	vector<int> prefix_sum(N+1);
+	int N; cin >> N;
+	vector<int> prefix_sum(N + 1);
 	for (int i = 1; i <= N; i++) {
 		int temp; cin >> temp;
 		prefix_sum[i] += prefix_sum[i - 1] + temp;
 	}
-	int ans = -INF;
-	for (int i = K; i <= N; i++) {
-		ans = max(ans, prefix_sum[i] - prefix_sum[i - K]);
+	int M; cin >> M;
+	while (M--) {
+		int st, ed; cin >> st >> ed;
+		cout << prefix_sum[ed] - prefix_sum[st - 1] <<"\n";
 	}
-	cout << ans;
 	return 0;
 }
