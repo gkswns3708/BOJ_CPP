620d31d53f38280c7fda75c348f104d09e52f0f5
Choi Han jun <gkswns3708@naver.com>
2021-03-20 02:32:59 +0900

    BaekJoon 2021번 최소 환승 경로

--- PS/BaekJoon 2021번 최소 환승 경로.cpp
+++ PS/BaekJoon 2021번 최소 환승 경로.cpp
@@ -6,45 +6,44 @@
 #define pii pair<int,int>
 using namespace std;
 
-int N, L,K;
+int N, L;
 
-vector<int> edge[200000 + 1];
-vector<int> dist(100000 + 100000 + 2,-1);
-bool visited[200000 + 2];
+vector<pair<int, int>> edge[200000 + 1];
+vector<int> dist(100000 + 100000 + 2, -1);
 
 int sol(int st, int ed) {
+	if (st == ed) return 0;
 	queue<int> que;
 	dist[st] = 0;
-	visited[1] = true;
 	que.push(st);
 	while (!que.empty()) {
 		int now = que.front();
 		que.pop();
-		if (now == N) break;
-		for (auto nxt : edge[now]) {
-			if (!visited[nxt]) {
-				visited[nxt] = 1;
-				dist[nxt] = dist[now] + 1;
+		for (auto [nxt, cost] : edge[now]) {
+			if (dist[nxt] != -1) continue;
+			else {
+				dist[nxt] = dist[now] + cost;
 				que.push(nxt);
 			}
 		}
 	}
-	if (N != 1 && dist[ed] == -1) return -1;
-	else return dist[ed]/2 + 1;
+	if (dist[ed] == -1) return -1;
+	else return dist[ed] - 1;
 }
 
 int32_t main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	cin >> N >> K >> L;
+	cin >> N >> L;
 	for (int i = 1; i <= L; i++) {
-		for (int j = 0; j < K; j++) {
-			int now; cin >> now;
-			edge[N + i].push_back(now);
-			edge[now].push_back( N + i);
+		int now;
+		while (cin >> now && now != -1) {
+			edge[N + i].push_back({ now,0 });
+			edge[now].push_back({ N + i,1 });
 		}
 	}
-	cout << sol(1, N);
+	int st, ed; cin >> st >> ed;
+	cout << sol(st, ed);
 	return 0;
 }
