bfdafe6680b2d86702c1287fbe68b98363a3e5c7
Choi Han jun <gkswns3708@naver.com>
2021-05-11 23:53:34 +0900

    BaekJoon 21275번 폰 호석만

--- PS/BaekJoon 21275번 폰 호석만.cpp
+++ PS/BaekJoon 21275번 폰 호석만.cpp
@@ -18,34 +18,34 @@ int32_t main() {
 	vector<pii> possiblea, possibleb;
 	int maxa = 0, maxb = 0;
 	for (auto now : a) {
-		if(NaN(now))maxa = max(maxa, int64_t((now - '0')) + 1);
+		if (NaN(now))maxa = max(maxa, int64_t((now - '0')) + 1);
 		else maxa = max(maxa, int64_t((now - 'a' + 10)) + 1);
 	}
 	for (auto now : b) {
 		if (now >= '0' && now <= '9')maxb = max(maxb, int64_t((now - '0')) + 1);
-		if (now >= 'a' && now <= 'z')maxb = max(maxb, int64_t((now - 'a' + 10))+ 1);
-	}
+		if (now >= 'a' && now <= 'z')maxb = max(maxb, int64_t((now - 'a' + 10)) + 1);
+	} 
 
-	for (int i = maxa; i <=36; i++) {
+	for (int i = maxa; i <= 36; i++) {
 		int num = 0;
 		bool flag = true;
 		for (int j = 0; j < a.length(); j++) {
-			if (num >= (LLONG_MAX - 1LL*(NaN(a[j]) ? a[j] - '0' : a[j] - 'a' + 10)) / i  ) {
+			if (num >= (1ULL << 63 - 1LL * (NaN(a[j]) ? a[j] - '0' : a[j] - 'a' + 10)) / i + (1ULL << 63 - 1LL * (NaN(a[j]) ? a[j] - '0' : a[j] - 'a' + 10)) % i) {
 				flag = false;
 				break;
 			}
 			num *= i;
 			if (NaN(a[j])) 	num += a[j] - '0';
 			else num += a[j] - 'a' + 10;
-			
+
 		}
-		if(flag) possiblea.push_back({ num ,i});
+		if (flag) possiblea.push_back({ num ,i });
 	}
 	for (int i = maxb; i <= 36; i++) {
 		int num = 0;
 		bool flag = true;
 		for (int j = 0; j < a.length(); j++) {
-			if (num >= LLONG_MAX - 1LL*((NaN(b[j]) ? b[j] - '0' : b[j] - 'a' + 10))/ i ) {
+			if (num >= 1ULL << 63 - 1LL * ((NaN(b[j]) ? b[j] - '0' : b[j] - 'a' + 10)) / i + 1ULL << 63 - 1LL * ((NaN(b[j]) ? b[j] - '0' : b[j] - 'a' + 10)) % i) {
 				flag = false;
 				break;
 			}
@@ -53,13 +53,13 @@ int32_t main() {
 			if (NaN(b[j])) num += b[j] - '0';
 			else num += b[j] - 'a' + 10;
 		}
-		if(flag) possibleb.push_back({ num , i });
+		if (flag) possibleb.push_back({ num , i });
 	}
 
 	bool flag = false;
 	vector<tiii> ans;
-	for (auto now : possiblea) 
- 		for (auto nxt : possibleb) 
+	for (auto now : possiblea)
+		for (auto nxt : possibleb)
 			if (now.first == nxt.first && now.second != nxt.second) ans.push_back({ now.first, now.second , nxt.second });
 
 	if (ans.size() == 0)cout << "Impossible\n";
