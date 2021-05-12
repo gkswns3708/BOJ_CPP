6f187dcc69b9b84583bf096c617440af212019e5
Choi Han jun <gkswns3708@naver.com>
2021-05-13 02:32:52 +0900

    BaekJoon 2379번 트리 탐색하기

--- PS/BaekJoon 2379번 트리 탐색하기.cpp
+++ PS/BaekJoon 2379번 트리 탐색하기.cpp
@@ -1,61 +1,60 @@
-#include<cstdio>
-#include<cstring>
 #include<iostream>
 #include<algorithm>
 using namespace std;
 //공부용 copy코드입니다.
 int t, len[2], a[30001], b[30001], fa[2][30001], num[2][30001];
 bool flag;
-char s[2][30001];
+string s[2];
 
 void findd(int u)
 {
 	int now = -1;
-	for (int i = 1; i <= len[u]; i++)
-		if (s[u][i] == '0')
-		{
-			fa[u][i] = now; now = i;
-		}
+	for (int i = 1; i <= len[u]; i++) {
+		//아래로 내려가는 코드
+		if (s[u][i] == '0')	fa[u][i] = now, now = i;
+		//위로 상승하는 코드(즉 계속해서 root가 변화하면서 계형을 가져감.)
 		else now = fa[u][now];
+	}
 }
 
 void cal(int u)
 {
 	int now = -1;
-	for (int i = 1; i <= len[0]; i++)
-		if (s[u][i] = '1')
-		{
+	for (int i = 1; i <= len[0]; i++) {
+		if (s[u][i] = '1'){
 			now = i;
-			while (now != -1)
-			{
-				num[u][now]++;
-				now = fa[u][now];
-			}
+			while (now != -1)num[u][now]++,now = fa[u][now];
 		}
+	}
 }
 
 int main()
 {
-	scanf("%d", &t);
+	cin >> t;
 	while (t--)
 	{
-		scanf("%s%s", s[0] + 1, s[1] + 1); flag = 0;
-		len[0] = strlen(s[0] + 1); len[1] = strlen(s[1] + 1);
-		if (len[0] != len[1])
-		{
-			puts("different"); continue;
-		}
+		flag = 0;
+		cin >> s[0] >> s[1]; 
+		//if (s[0].length() != s[1].length()){
+		//	cout << "0\n"; continue;
+		//}
+		//최종 자손들은 -1을 가리키게 초기화
 		memset(fa[0], -1, sizeof(fa[0]));
 		memset(fa[1], -1, sizeof(fa[1]));
+
 		memset(num[0], 0, sizeof(num[0]));
 		memset(num[1], 0, sizeof(num[1]));
-		findd(0); cal(0); findd(1); cal(1);
+		findd(0); 
+		cal(0); 
+		findd(1); 
+		cal(1);
 		sort(num[0] + 1, num[0] + len[0] + 1);
 		sort(num[1] + 1, num[1] + len[1] + 1);
 		for (int i = 1; i <= len[0]; i++)
-			if (num[0][i] != num[1][i])
-			{
-				cout << "0\n"; flag = 1; break;
+			//가지고 있는 순서가 달라지면 error
+			if (num[0][i] != num[1][i]){
+				cout << "0\n"; 
+				flag = 1; break;
 			}
 		if (!flag) cout << "1\n";
 	}
--- PS/practice.cpp
+++ PS/practice.cpp
@@ -5,40 +5,10 @@
 using namespace std;
 
 int main() {
-
-	ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-
-	int T;
-	int N, M, K, H;
-	int X, Y;
-
-	cin >> T;
-
-	vector<int> v(T);
-
-	for (int i = 0; i < T; i++)
-	{
-		cin >> v[i];
-	}
-
-	sort(v.begin(), v.end());
-
-	cin >> T;
-
-	vector<int> answer(T);
-
-	for (int i = 0; i < T; i++)
-	{
-		cin >> K;
-		vector<int>::iterator upper = upper_bound(v.begin(), v.end(), K);
-		auto lower = lower_bound(v.begin(), v.end(), K);
-		answer[i] = upper - lower;
-	}
-
-	for (auto n : answer)
-	{
-		cout << n << " ";
-	}
-
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int a; cin >> a;
+	cout << a <<"\n"; //"\n"
 	return 0;
 }
