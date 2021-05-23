c21b9b70973cc64569014a8cbb8459fec86ae481
Choi Han jun <gkswns3708@naver.com>
2021-05-23 22:00:56 +0900

    BaekJoon 히스토그램에서 가장 큰 직사각형 찾기

---
+++ PS/BaekJoon 6549번 히스토그램에서 가장 큰 직사각형.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+#define int int64_t
+#define pii pair<int,int>
+#define tiii tuple<int,int,int>
+using namespace std;
+
+
+int32_t main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	while (1) {
+		int arr[100000 + 1] = { 0, };
+		int N; cin >> N;
+		if (N == 0) break;
+		stack<int> st;
+		int ans = 0;
+		for (int i = 0; i < N; i++) {
+			cin >> arr[i];
+			while (!st.empty() && arr[st.top()] > arr[i]) {
+				int h = arr[st.top()];
+				int w = i;
+				st.pop();
+				if (!st.empty()) w = i - st.top() - 1;
+				ans = max(ans, h * w);
+			}
+			st.push(i);
+		}
+		while (!st.empty()) {
+			int h = arr[st.top()];
+			int w = N;
+			st.pop();
+			if (!st.empty())w = N - st.top() - 1;
+			ans = max(ans, h * w);
+		}
+		cout << ans <<"\n";
+	}
+	return 0;
+}
