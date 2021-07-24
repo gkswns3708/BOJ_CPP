98b0526ddf614febc3224741301656df29fefa90
Choi Han jun <gkswns3708@naver.com>
2021-07-24 16:38:38 +0900

    BaekJoon 1321번 군인

---
+++ PS/BaekJoon 1321번 군인.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#define int int64_t
+#define pii pair<int,int>
+#define tiii tuple<int,int,int>
+using namespace std;
+
+struct ST {
+	vector<int> tree;
+	int sz;
+	ST(int n, vector<int>& arr) {
+		int h = (int)ceil(log2(n));
+		sz = 1LL << h;
+		tree.resize(sz << 1);
+		for (int i = 0; i < arr.size(); i++) tree[i + sz] = arr[i];
+		for (int i = sz-1 ; i >= 0; i--) tree[i] = tree[i * 2] + tree[i * 2 + 1];
+	}
+
+	void update(int i, int x) {
+		int node = i + sz-1;
+		tree[node] += x;
+		while (node >>= 1) tree[node] = tree[node << 1] + tree[node << 1 | 1];
+	}
+	int query(int x) {
+		int i = 1;
+		while (i <= (sz-1)) {
+			int l = tree[i << 1], r = tree[i << 1 | 1];
+			if (l >= x) i <<= 1;//왼쪽에 있는 경우
+			else i = i << 1 | 1, x -= l; //i가 오른쪽으로 가고 x는 원래 왼쪽의 사람들 만큼 빠짐.
+		}
+		return i - (sz-1);
+	}
+};
+
+
+
+int32_t main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int N; cin >> N;
+	vector<int> vec(N);
+	for (int i = 0; i < N; i++) cin >> vec[i];
+	ST st(N, vec);
+	int M; cin >> M;
+	for (int i = 0; i < M; i++) {
+		int a, b; cin >> a >> b;
+		if (a & 1) {
+			int c; cin >> c;
+			st.update(b, c);
+		}
+		else cout << st.query(b) << "\n";
+	}
+	return 0;
+}
--- PS/practice.cpp
+++ PS/practice.cpp
@@ -1,16 +1,46 @@
-#include <bits/stdc++.h>
-#define int int64_t
-#define pii pair<int,int>
-#define tiii tuple<int,int,int>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#define int long long
+#define pii pair<int , int>
 using namespace std;
+
+int arr[1000 + 2][1000 + 2];
+pii check[1000 + 1][1000 + 1];
+int ans[1000 + 2][1000 + 2];
 
 int32_t main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	string s;
-	getline(cin, s);
-	cin.ignore();
+
+
+	int N, M, R; cin >> N >> M >> R;
+	vector<vector<int>> vec(min(N, M) / 2 + 1);
+	for (int i = 1; i <= N; i++) for (int j = 1; j <= M; j++) cin >> arr[i][j];
+
+	function <void(int)> search = [&](int x) -> void {
+		pii st = { x,x };
+		int cnt = 0;
+		while (st.second <= M + 1 - x) 
+			check[st.first][st.second] = { x,cnt++ }, vec[x].push_back(arr[st.first][st.second++]);
+		st.second--, st.first++;
+		while (st.first <= N + 1 - x) 
+			check[st.first][st.second] = { x,cnt++ }, vec[x].push_back(arr[st.first++][st.second]);
+		st.first--, st.second--;
+		while (st.second >= x) 
+			check[st.first][st.second] = { x,cnt++ }, vec[x].push_back(arr[st.first][st.second--]);
+		st.second++, st.first--;
+		while (st.first >= x + 1) 
+			check[st.first][st.second] = { x,cnt++ }, vec[x].push_back(arr[st.first--][st.second]);
+	};
+	for (int i = 1; i <= min(N, M) / 2; i++) search(i);
+	for (int i = 1; i <= N; i++) for (int j = 1; j <= M; j++) ans[i][j] = vec[check[i][j].first][(check[i][j].second + R) % vec[check[i][j].first].size()];;
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= M; j++) cout << ans[i][j] << " ";
+		cout << '\n';
+	}
 
 	return 0;
 }
