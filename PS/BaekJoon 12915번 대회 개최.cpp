b8dab8b528270d9b3c2e95cb34d6755815e9faac
HJ <gkswns3708@naver.com>
2021-10-16 00:27:47 +0900

    BaekJoon 12915번 대회 개최

--- PS/BaekJoon 12915번 대회 개최.cpp
+++ PS/BaekJoon 12915번 대회 개최.cpp
@@ -5,10 +5,11 @@
 using namespace std;
 
 bool possible(int E,int EM,int M,int MH,int H,int mid) {
-    // mid = 현재 벌어질 대회의 수.
-    int e = E > mid ? 0 : mid - E; //EM으로 넘어갈 대회 수 
-    int h = H > mid ? 0 : mid - H; //MH로 넘어갈 대회 수
-    int em = EM > e ? 0 :
+    int _em = E > mid ? EM : E + EM - mid;
+    int _mh = H > mid? MH : H + MH - mid;
+    int _m = M;
+    if (_em < 0 || _mh < 0) return false;
+    return _em + _mh + _m >= mid;
 }
 
 int32_t main()
@@ -17,7 +18,7 @@ int32_t main()
     cin.tie(0);
     cout.tie(0);
     int E, EM, M, MH, H; cin >> E >> EM >> M >> MH >> H;
-    int lo = 0, hi = 200000;
+    int lo = 0, hi = 166667;
     while (lo + 1 < hi) {
         int mid = (lo + hi) / 2;
         if (possible(E, EM, M, MH, H, mid)) lo = mid;
